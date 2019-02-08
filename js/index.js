8c2e9f3b264666e3c21cd4fc484fba92e8b80bb5
Vitaliy <vitaliyvak@gmail.com>
2019-02-08 13:29:18 +0000

    on fire

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const display = document.querySelector('.calculator input[name=display]');
+
+document.querySelectorAll('.digits button, .opers button').forEach(btn=>btn.addEventListener('click', digitOperPressed));
+
+function digitOperPressed(e) {
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.result').addEventListener('click', function() {
+    display.value = eval(display.value);
+})
+document.querySelector('.clear').addEventListener('click', function() {
+    display.value = ('');
+});
+document.querySelector('.sqrt').addEventListener('click', function() {
+    display.value = eval(Math.sqrt(display.value));
+});
+document.querySelector('.del').addEventListener('click', function() {
+    display.value = display.value.substring(0, display.value.length - 1);
+});
